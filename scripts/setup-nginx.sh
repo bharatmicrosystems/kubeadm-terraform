930ab1993b0fd56c14f003e59ceda0e3fae606a7
Agarwal <gaurav.a.agarwal@capgemini.com>
2020-04-17 16:58:00 +0100

    Updated setup nginx

--- scripts/setup-nginx.sh
+++ scripts/setup-nginx.sh
@@ -1,12 +1,13 @@
 loadbalancers=$1
 masters=$2
 etcds=$3
+nodes=$4
 internal_vip=$4
 external_vip=$5
 for instance in $(echo $loadbalancers | tr ',' ' '); do
   ZONE=`gcloud compute instances list --filter="name=${instance}"| grep ${instance} | awk '{ print $2 }'`
   gcloud compute scp --zone=$ZONE --internal-ip configure-nginx.sh $instance:~/
-  gcloud compute ssh --zone=$ZONE --internal-ip $instance -- "cd ~/ && sh -x configure-nginx.sh $masters $etcds"
+  gcloud compute ssh --zone=$ZONE --internal-ip $instance -- "cd ~/ && sh -x configure-nginx.sh $masters $etcds $nodes"
 done
 echo 'Setting up HA between NGINX Load Balancers'
 sh -x setup-gcp-failoverd.sh -i $internal_vip -e $external_vip -l $loadbalancers -c "k8scluster" -h ":8080\/nginx_status"
